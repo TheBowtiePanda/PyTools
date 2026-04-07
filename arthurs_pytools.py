3aa1caafa934b950859d27b320f7a6a34b56960f
TheBowtiePanda <Arthur.c.cressy@gmail.com>
2026-04-07 14:20:24 -0600

    added some comments

--- arthurs_pytools.py
+++ arthurs_pytools.py
@@ -1,27 +1,37 @@
 import random
 
 class arthurs_pytools:
+    # TheBowtiePanda
     def bare_input(name):
+        # makes a user input
         user_input = input(f'{name}: ')
+        #removes punctuation and spaces from the input
         user_input = user_input.lower().replace("!", "").replace(".", "").replace("?", "").replace(",", "").replace("'", "").replace(" ", "")
+        #returns user input
         return user_input
 
 
-
+    # TheBowtiePanda
     def random_list(length, min_value, max_value):
         ran_list = []
+        #loops (length) times
         while length > 0:
+            #make a random number
             random_int = random.randint(min_value, max_value)
+            #add that number to a list
             ran_list.append(random_int)
             length -= 1
         return ran_list
 
 
-
+    # TheBowtiePanda
     def dice_roll(sides, times_rolled=1):
         rolls = []
+        #loops (times_rolled) times. default of 1
         while 0 < times_rolled:
+            #pick a random side of the die
             side = random.choice(sides)
+            #add that to a list
             rolls.append(side)
             times_rolled -= 1
         return rolls
